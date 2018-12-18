77276d66abf8d7add2139a73ab9885c8722e9390
khushkomalsingh <45960860+khushkomalsingh@users.noreply.github.com>
2018-12-17 23:04:23 -0800

    Create kh.php
    sdfsf

---
+++ kh.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div style="width: 1000px auto;height: 60px;background-color: pink"></div>
+
+
+<div style="width: 1000px auto;height: 530px;background-color: grey ">
+	
+<div style="width: 260px;height: 320px;border: 2px solid black;">
+	<h4 style="margin-left: 70px;">Login here</h4>
+<br>
+Username:<input type="text" name="text">
+<br>
+<br>
+
+Password:<input type="text" name="text">
+<br>
+<br>
+<button style="margin-left: 100px;border-radius: 5px;background-color: skyblue;color: black">login</button>
+
+</div>
+
+</div>
+
+<div style="width: 1000px auto;height: 60px;background-color: pink"></div>
+
+</body>
+</html>
